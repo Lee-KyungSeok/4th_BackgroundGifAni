9b21ea2345603e6da75aeab01688f3fa96fcd8ef
javafa <nigne2016@gmail.com>
2017-11-30 18:48:51 +0900

    init

--- app/src/main/java/net/flow9/backgroundgifani/MainActivity.java
+++ app/src/main/java/net/flow9/backgroundgifani/MainActivity.java
@@ -33,22 +33,29 @@ public class MainActivity extends AppCompatActivity {
                 .load(R.drawable.giphy)
                 .into(ivImg);
 
+        // 최상위 레이아웃
         final FrameLayout container = findViewById(R.id.container);
 
+        // 최상위 레이아웃에 변경사항 체크
         container.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+                // 입력필드들이 있는 레이아웃 가져오기
                 View view = findViewById(R.id.keyboard);
-                int marginBottom;
+                int keyboard_height;
                 Rect outGlobalRect = new Rect();
                 Rect outWindowRect = new Rect();
                 DisplayMetrics metrics = new DisplayMetrics();
                 getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                // 실제 레이아웃 사이즈 가져오기
                 container.getGlobalVisibleRect(outGlobalRect);
+                // 보여지는 (키보드가 가린부분 제외) 레이아웃 사이즈 가져오기
                 container.getWindowVisibleDisplayFrame(outWindowRect);
-                marginBottom = outGlobalRect.bottom - outWindowRect.bottom;
+                // 실제 레이아웃에서 보여지는 부분(키보드 크기)만 빼면 키보드의 높이가 나온다
+                keyboard_height = outGlobalRect.bottom - outWindowRect.bottom;
                 FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.BOTTOM);
-                layoutParams.setMargins(0, 0, 0, marginBottom);
+                // 키보드 높이만큼 아래쪽에 마진값을 줘서 레이아웃을 밀어 올린다.
+                layoutParams.setMargins(0, 0, 0, keyboard_height);
                 view.setLayoutParams(layoutParams);
             }
         });
